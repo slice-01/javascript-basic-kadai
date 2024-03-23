d5962e09401f512c1b052665bd8ef69b0e41da81
slice-01 <lightning.sou.o@gmail.com>
2024-03-23 22:50:55 +0900

    kadai_005追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+//体重：68kg
+let bodyWaight = 68;
+
+//身長：1.7m
+let bodyHeight = 1.7;
+
+//計算式：[体重(kg)]÷[身長(m)×身長(m)]wait = 68
+let bmi;
+bmi = bodyWaight / bodyHeight * bodyHeight;
+console.log(bmi);
